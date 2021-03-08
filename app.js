7f7cebb9ee29fed49f6b1ebcf492259713b6629b
Nadav <nadavcohen306@gmail.com>
2021-03-08 17:57:53 +0200

    start

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+
+app.get("/", function(req,res){
+  res.send("Hello");
+});
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
