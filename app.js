a49f77d521beb110cee8ee51f34837746faa2fba
liel <lielsananes8@gmail.com>
2021-03-09 23:41:56 +0200

    update

--- app.js
+++ app.js
@@ -55,22 +55,23 @@ const a_plus = new Type({type: "A+",name:"Aplus" ,quantity: 5});
 const b_plus = new Type({type: "B+",name:"Bplus", quantity: 5});
 const ab_plus = new Type({type: "AB+",name:"ABplus", quantity: 5});
 const o_plus = new Type({type: "O+",name:"Oplus", quantity: 5});
-const bloodTypes = [a_plus,b_plus,ab_plus,o_plus];
-const a_plus = new Type({type: "A+",name:"Aplus" ,quantity: 0, donate: ["A+","AB+"], receive: ["A+","A-","O+","O-"]});
-const b_plus = new Type({type: "B+",name:"Bplus", quantity: 0, donate: ["B+","AB+"], receive: ["B+","B-","O+","O-"]});
-const ab_plus = new Type({type: "AB+",name:"ABplus", quantity: 0, donate: ["AB+"], receive: ["A+","B+","AB+","O+","A-","B-","AB-","O-"]});
-const o_plus = new Type({type: "O+",name:"Oplus", quantity: 0, donate: ["A+","B+","AB+","O+"], receive: ["O+","O-"]});
-const a_minus = new Type({type: "A-",name:"Aminus" ,quantity: 0, donate: ["A+","A-","AB+","AB-"], receive: ["A-","O-"]});
-const b_minus = new Type({type: "B-",name:"Bminus", quantity: 0, donate: ["B+","B-","AB+","AB-"], receive: ["B-","O-"]});
 const ab_minus = new Type({type: "AB-",name:"ABminus", quantity: 0, donate: ["AB+","AB-"], receive: ["A-","B-","AB-","O-"]});
-const o_minus = new Type({type: "O-",name:"Ominus", quantity: 0, donate: ["A+","B+","AB+","O+","A-","B-","AB-","O-"], receive: ["O-"]});
+const a_plus = new Type({type: "A+",name:"Aplus" ,quantity: 50, donate: ["A+","AB+"], receive: ["A+","A-","O+","O-"]});
+const b_plus = new Type({type: "B+",name:"Bplus", quantity: 50, donate: ["B+","AB+"], receive: ["B+","B-","O+","O-"]});
+const ab_plus = new Type({type: "AB+",name:"ABplus", quantity: 50, donate: ["AB+"], receive: ["A+","B+","AB+","O+","A-","B-","AB-","O-"]});
+const o_plus = new Type({type: "O+",name:"Oplus", quantity: 50, donate: ["A+","B+","AB+","O+"], receive: ["O+","O-"]});
+const a_minus = new Type({type: "A-",name:"Aminus" ,quantity: 50, donate: ["A+","A-","AB+","AB-"], receive: ["A-","O-"]});
+const b_minus = new Type({type: "B-",name:"Bminus", quantity: 50, donate: ["B+","B-","AB+","AB-"], receive: ["B-","O-"]});
+const ab_minus = new Type({type: "AB-",name:"ABminus", quantity: 50, donate: ["AB+","AB-"], receive: ["A-","B-","AB-","O-"]});
+const o_minus = new Type({type: "O-",name:"Ominus", quantity: 50, donate: ["A+","B+","AB+","O+","A-","B-","AB-","O-"], receive: ["O-"]});
 const bloodTypes = [a_plus,b_plus,ab_plus,o_plus,a_minus,b_minus,ab_minus,o_minus];
 Type.insertMany(bloodTypes, function(err,docs){
 });
 
 
+
 app.get("/", function(req,res){
-  res.render("recept_normal",{});
+  
   Type.find({}, function(err,foundItems){
     if(!err){
       if(foundItems.length === 0){
@@ -88,7 +89,6 @@ app.get("/", function(req,res){
       } else {
         res.render("recept_normal",{blood: foundItems});
       }
-    } else {
     } 
     else {
       console.log(err);
@@ -96,7 +96,6 @@ app.get("/", function(req,res){
   });
 });
 
-app.post("/addBlood", function(req,res){
 app.post("/addBlood",async function(req,res){
   const bloodUnits_Aplus = parseInt(req.body.Aplus);
   const bloodUnits_Bplus = parseInt(req.body.Bplus);
@@ -126,27 +125,35 @@ app.post("/addBlood",async function(req,res){
   res.render("recept_normal",{blood: updatedBlood});
 });
 
-app.post("/giveBlood", async function(req,res){
-  const bloodUnits_Aplus = parseInt(req.body.Aplus);
-  const bloodUnits_Bplus = parseInt(req.body.Bplus);
-  const bloodUnits_ABplus = parseInt(req.body.ABplus);
-  const bloodUnits_Oplus = parseInt(req.body.Oplus);
-  const oldQuantity_Aplus = await Type.findOne({type: "A+"}, 'quantity -_id').exec();
-  const oldQuantity_Bplus = await Type.findOne({type: "B+"}, 'quantity -_id').exec();
-  const oldQuantity_ABplus = await Type.findOne({type: "AB+"}, 'quantity -_id').exec();
-  const oldQuantity_Oplus = await Type.findOne({type: "O+"}, 'quantity -_id').exec();
-  await Type.findOneAndUpdate({type: "A+"}, {quantity: oldQuantity_Aplus.quantity - bloodUnits_Aplus}).exec();
-  await Type.findOneAndUpdate({type: "B+"}, {quantity: oldQuantity_Bplus.quantity - bloodUnits_Bplus}).exec();
-  await Type.findOneAndUpdate({type: "AB+"}, {quantity: oldQuantity_ABplus.quantity - bloodUnits_ABplus}).exec();
-  await Type.findOneAndUpdate({type: "O+"}, {quantity: oldQuantity_Oplus.quantity - bloodUnits_Oplus}).exec();
-  //take the blood type object from database according to what user chose in the combobox
-  const requestedBloodType = await Type.findOne({type: req.body.bloodType}).exec();
-  //the amount that the user typed in the input
-  const requestedAmount = req.body.amount;
 
-  const updatedBlood = await Type.find({}).exec();
-  res.render("recept_normal",{blood: updatedBlood});
+app.post("/giveBlood",async function(req,res){
+  res.render("bloodList",{blood_list: ["A+","B+"]});
 });
+
+// app.post("/giveBlood",function(req,res) {
+//   // const bloodUnits_Aplus = parseInt(req.body.Aplus);
+//   // const bloodUnits_Bplus = parseInt(req.body.Bplus);
+//   // const bloodUnits_ABplus = parseInt(req.body.ABplus);
+//   // const bloodUnits_Oplus = parseInt(req.body.Oplus);
+//   // const oldQuantity_Aplus = await Type.findOne({type: "A+"}, 'quantity -_id').exec();
+//   // const oldQuantity_Bplus = await Type.findOne({type: "B+"}, 'quantity -_id').exec();
+//   // const oldQuantity_ABplus = await Type.findOne({type: "AB+"}, 'quantity -_id').exec();
+//   // const oldQuantity_Oplus = await Type.findOne({type: "O+"}, 'quantity -_id').exec();
+//   // await Type.findOneAndUpdate({type: "A+"}, {quantity: oldQuantity_Aplus.quantity - bloodUnits_Aplus}).exec();
+//   // await Type.findOneAndUpdate({type: "B+"}, {quantity: oldQuantity_Bplus.quantity - bloodUnits_Bplus}).exec();
+//   // await Type.findOneAndUpdate({type: "AB+"}, {quantity: oldQuantity_ABplus.quantity - bloodUnits_ABplus}).exec();
+//   // await Type.findOneAndUpdate({type: "O+"}, {quantity: oldQuantity_Oplus.quantity - bloodUnits_Oplus}).exec();
+//   // //take the blood type object from database according to what user chose in the combobox
+//   // const requestedBloodType = await Type.findOne({type: req.body.bloodType}).exec();
+//   // //the amount that the user typed in the input
+//   // const requestedAmount = req.body.amount;
+
+//   // const updatedBlood = await Type.find({}).exec();
+//   // res.render("recept_normal",{blood: updatedBlood});
+  
+// });
+
+
 
 
 
