8c3cf97780ef41b9c2f521ebbab5db3610607c42
Nadav <nadavcohen306@gmail.com>
2021-03-09 18:06:50 +0200

    updated provide

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ app.use(express.static("public"));
 
 //mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connect('mongodb://localhost:27017/becsDB', {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://admin-nadav:123@cluster0.ohzh4.mongodb.net/becsDB', {useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
 
 const bloodSchema = {
   type: String,
+  name: String,
   quantity: Number
 };
 
@@ -45,10 +47,10 @@ const oPlus = {
   receive: ["O+"]
 };
 
-const a_plus = new Type({type: "A+", quantity: 5});
-const b_plus = new Type({type: "B+", quantity: 5});
-const ab_plus = new Type({type: "AB+", quantity: 5});
-const o_plus = new Type({type: "O+", quantity: 5});
+const a_plus = new Type({type: "A+",name:"Aplus" ,quantity: 5});
+const b_plus = new Type({type: "B+",name:"Bplus", quantity: 5});
+const ab_plus = new Type({type: "AB+",name:"ABplus", quantity: 5});
+const o_plus = new Type({type: "O+",name:"Oplus", quantity: 5});
 const bloodTypes = [a_plus,b_plus,ab_plus,o_plus];
 
 app.get("/", function(req,res){
@@ -68,7 +70,7 @@ app.get("/", function(req,res){
           }
         });
       } else {
-        res.render("recept_normal",{});
+        res.render("recept_normal",{blood: foundItems});
       }
     } else {
       console.log(err);
@@ -77,23 +79,39 @@ app.get("/", function(req,res){
 });
 
 app.post("/addBlood", function(req,res){
+app.post("/addBlood",async function(req,res){
   const bloodUnits_Aplus = parseInt(req.body.Aplus);
   const bloodUnits_Bplus = parseInt(req.body.Bplus);
   const bloodUnits_ABplus = parseInt(req.body.ABplus);
   const bloodUnits_Oplus = parseInt(req.body.Oplus);
-  Type.findOne({type: "A+"}, function(err,foundItem){
-    if(!err){
-      Type.updateOne({_id: foundItem._id}, {quantity: parseInt(foundItem.quantity) + bloodUnits_Aplus},function(err){
-        if(!err){
-          console.log("updated");
-          res.render("recept_normal",{});
-        } else {
-          console.log(err);
-        }
-      });
-    }
-  });
-})
+  const oldQuantity_Aplus = await Type.findOne({type: "A+"}, 'quantity -_id').exec();
+  const oldQuantity_Bplus = await Type.findOne({type: "B+"}, 'quantity -_id').exec();
+  const oldQuantity_ABplus = await Type.findOne({type: "AB+"}, 'quantity -_id').exec();
+  const oldQuantity_Oplus = await Type.findOne({type: "O+"}, 'quantity -_id').exec();
+  await Type.findOneAndUpdate({type: "A+"}, {quantity: bloodUnits_Aplus + oldQuantity_Aplus.quantity}).exec();
+  await Type.findOneAndUpdate({type: "B+"}, {quantity: bloodUnits_Bplus + oldQuantity_Bplus.quantity}).exec();
+  await Type.findOneAndUpdate({type: "AB+"}, {quantity: bloodUnits_ABplus + oldQuantity_ABplus.quantity}).exec();
+  await Type.findOneAndUpdate({type: "O+"}, {quantity: bloodUnits_Oplus + oldQuantity_Oplus.quantity}).exec();
+  const updatedBlood = await Type.find({}).exec();
+  res.render("recept_normal",{blood: updatedBlood});
+});
+
+app.post("/giveBlood", async function(req,res){
+  const bloodUnits_Aplus = parseInt(req.body.Aplus);
+  const bloodUnits_Bplus = parseInt(req.body.Bplus);
+  const bloodUnits_ABplus = parseInt(req.body.ABplus);
+  const bloodUnits_Oplus = parseInt(req.body.Oplus);
+  const oldQuantity_Aplus = await Type.findOne({type: "A+"}, 'quantity -_id').exec();
+  const oldQuantity_Bplus = await Type.findOne({type: "B+"}, 'quantity -_id').exec();
+  const oldQuantity_ABplus = await Type.findOne({type: "AB+"}, 'quantity -_id').exec();
+  const oldQuantity_Oplus = await Type.findOne({type: "O+"}, 'quantity -_id').exec();
+  await Type.findOneAndUpdate({type: "A+"}, {quantity: oldQuantity_Aplus.quantity - bloodUnits_Aplus}).exec();
+  await Type.findOneAndUpdate({type: "B+"}, {quantity: oldQuantity_Bplus.quantity - bloodUnits_Bplus}).exec();
+  await Type.findOneAndUpdate({type: "AB+"}, {quantity: oldQuantity_ABplus.quantity - bloodUnits_ABplus}).exec();
+  await Type.findOneAndUpdate({type: "O+"}, {quantity: oldQuantity_Oplus.quantity - bloodUnits_Oplus}).exec();
+  const updatedBlood = await Type.find({}).exec();
+  res.render("recept_normal",{blood: updatedBlood});
+});
 
 
 
